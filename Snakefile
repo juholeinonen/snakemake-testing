ba3b60af2b1fa89e78dc647d7f8c06b9d6752fb3
juholeinonen <juho.leinonen@aalto.fi>
2021-07-27 11:48:07 +0300

    before testing

--- Snakefile
+++ Snakefile
@@ -4,10 +4,38 @@ rule all:
   input:
     expand("{speaker}_wav-list", speaker=config["speakers"].values())
 
-rule select_speaker_files:
+rule select_speaker_files_from_wav_list:
   input:
     wav=config["files"]["wavs"]
   output:
     speaker="{speaker}_wav-list"
   shell:
     'grep "{wildcards.speaker}" {input.wav} > {output.speaker}'
+
+rule select_speaker_files_from_trn_list:
+  input:
+    trn=config["files"]["trn"]
+  output:
+    speaker="{speaker}_trn-list"
+  shell:
+    'grep "{wildcards.speaker}" {input.trn} > {output.speaker}'
+
+rule select_x_second_amount_of_wavs:
+  input:
+    wav="{speaker}_wav-list"
+    script="scripts/takeXseconds.py"
+    seconds=config["params"]["seconds"]
+  output:
+    wav="{speaker}_{input.seconds}_seconds_wav_list"
+  shell:
+    "{input.script} {input.wav} {output.wav} {input.seconds}"
+
+rule select_transcripts_for_seconds_wav_list:
+  input:
+    wav="{speaker_seconds}_seconds_wav_list"
+    speaker="{speaker}_trn-list"
+    script="scripts/fix_trn.py"
+  output:
+    trn="{speaker_seconds}_seconds_trn_list"
+  shell:
+   "{input.script} {input.speaker} {output.trn} {input.wav}" 
--- scripts/fix_trn.py
+++ scripts/fix_trn.py
@@ -3,22 +3,19 @@
 import os
 import sys
 
-def main(full_trn_list, filtered_trn_list, wav_list):
-    grepped_trn_name = "grepped_trn_list"
-    trn_name = "tmp_trn_list"
-    wav_scp_name = os.path.join(output_dir, "wav.scp")
+def main(grepped_trn_list, filtered_trn_list, wav_list):
 
     utterance_IDs = [] 
 
-    with open(grepped_trn_name, "r", encoding="utf-8") as greppedlist_file,\
-        open(wav_scp_name, "r", encoding="utf-8") as wavlist_file,\
-        open(trn_name, "w", encoding="utf-8") as trn_file:
+    with open(grepped_trn_list, "r", encoding="utf-8") as grepped_trn_file,\
+        open(wav_list, "r", encoding="utf-8") as wav_list_file,\
+        open(filtered_trn_list, "w", encoding="utf-8") as trn_file:
 
-        for line in wavlist_file:
+        for line in wav_list_file:
             line_ID = os.path.splitext(os.path.basename(line))[0]
             utterance_IDs.append(line_ID)
 
-        for line in greppedlist_file:
+        for line in grepped_trn_file:
             line_parts = line.strip().split('(')
 
             line_ID = line_parts[1][:-1]
@@ -26,4 +23,4 @@ def main(full_trn_list, filtered_trn_list, wav_list):
                 trn_file.write(line)
 
 if __name__ == "__main__":
-	main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2], sys.argv[3]))
